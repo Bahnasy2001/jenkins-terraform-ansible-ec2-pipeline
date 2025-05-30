964585028410097fa85d5cd1d6ae40ed7e877d58
Hassan Bahnasy <hassanbahnasy872@gmail.com>
2025-05-30 23:23:20 +0300

    Update AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-03d8b47244d950bbb" 
+  ami           = "ami-02b7d5b1e55a7b5f1" 
   instance_type = "t2.micro"
   key_name      = aws_key_pair.jenkins_key.key_name
 
